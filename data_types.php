8dc49961f465e970efbd027ca38c49b9c4012c57
Jose Hernandez <reyes_7374@outlook.es>
2025-01-28 18:53:10 -0600

    modificacion al archivo data_types.php

--- data_types.php
+++ data_types.php
@@ -1,53 +1,46 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
-    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="">
-    <style>
-        * {
-            margin: 0;
-            box-sizing: border-box;
-        }
+/**
+ * Scalar
+ */
+echo "Integers\n";
+// echo '<br>';
+$count = 0;
+$max = 1000;
+$page_size = 10;
+echo $count . "\n";
+echo $max . "\n";
+echo $page_size . "\n";
 
-        p {
-            margin-bottom: 4px;
-        }
-    </style>
-</head>
+echo  "Float\n";
+$price = 10.25;
+$tax = 0.88;
+echo $price . "\n";
+echo $tax . "\n";
 
-<body>
-    <?php
-    /*
-    Scalar 
-        * boolean
-        * integer
-        * float 
-        * string
-    Compound
-        * Array
-        * Object
-    Special
-        * Null
-        *Resource
-    */
-    echo '<p> Integers </p>';
-    // echo '<br>';
-    $count = 0;
-    $max = 1000;
-    $page_size = 10;
-    echo '<p>' . $count . ' ' . $max . ' ' . $page_size . '</p>';
-    echo '<hr/>';
+echo "Boolean\n";
+$is_admin = true;
+$is_user_logged_in = false;
+echo $is_admin . "\n";
+// echo $is_user_logged_in . "\n";
 
-    echo  '<p> Float</p>';
-    $price = 10.25;
-    $tax = 0.88;
-    echo '<p>' . $price . '</p>';
-    echo '<p>' . $tax . '</p>';
-    echo '<hr/>';
+echo "String\n";
+$str = "php scalar type";
+$message = "PHP data types";
+echo $str . "\n";
+echo $message . "\n";
 
-    ?>
-</body>
-
-</html>
+/**
+ * Compound
+ */
+echo "Arrays";
+$carts = ['laptop', 'mouse', 'keyboard'];
+echo "Indexed array.\n";
+var_dump($carts);
+echo "Associative arrays.\n";
+$prices = [
+    "laptop" => 1000,
+    "mouse" => 50,
+    "keyboard" => 120
+];
+var_dump($prices);
